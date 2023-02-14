dc0abc6c048afb0fc2ea99b35177e27bf5fa2578
LukasMut <muttenthaler@cbs.mpg.de>
2023-02-14 11:30:54 +0100

    implemented contrastive loss for probing

---
+++ utils/probing/contrastive_loss.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+Tensor = torch.Tensor
+
+
+class ContrastiveLoss(nn.Module):
+    def __init__(self, temperature: float = 1.0) -> None:
+        super(ContrastiveLoss, self).__init__()
+        self.temperature = temperature
+        self.c_entropy = nn.CrossEntropyLoss()
+
+    @staticmethod
+    def mask_diagonal(dots: Tensor) -> Tensor:
+        return dots[~torch.eye(dots.shape[0], dtype=bool)].reshape(dots.shape[0], -1)
+
+    def get_teacher_distribution(self, teacher_similarities: Tensor) -> Tensor:
+        return F.softmax(self.mask_diagonal(teacher_similarities), dim=-1)
+
+    def cross_entropy_loss(
+        self, teacher_similarities: Tensor, student_similarities: Tensor
+    ) -> Tensor:
+        p = self.get_teacher_distribution(teacher_similarities)
+        q_unnormalized = self.mask_diagonal(student_similarities) / self.temperature
+        return self.c_entropy(q_unnormalized, p)
+
+    def forward(
+        self, teacher_similarities: Tensor, student_similarities: Tensor
+    ) -> Tensor:
+        return self.cross_entropy_loss(teacher_similarities, student_similarities)
