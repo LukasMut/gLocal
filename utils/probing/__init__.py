23ab067f7faef9b6df6f3aaa3c99c49fa26c10d7
LukasMut <muttenthaler@cbs.mpg.de>
2023-02-14 17:30:55 +0100

    fixed typo

--- utils/probing/__init__.py
+++ utils/probing/__init__.py
@@ -4,5 +4,5 @@ from .global_probe import GlobalProbe
 from .glocal_probe import GlocalProbe
 from .helpers import (get_temperature, load_model_config, load_triplets,
                       partition_triplets, repeat, standardize,
-                      zip_train_batches)
+                      zip_batches)
 from .triplet_loss import TripletLoss
