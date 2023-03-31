bb7268f836c4f402d6bffda8cb55e097f492631c
Jonas Dippel <jonas.dippel@gmx.de>
2023-03-31 10:02:08 +0200

    added cache to ad implementation

--- downstream/anomaly_detection/evaluation.py
+++ downstream/anomaly_detection/evaluation.py
@@ -7,6 +7,7 @@ from .dtd import ADDTD
 from .fine_grained import ADFlowers
 from thingsvision import get_extractor
 import torch.nn.functional as F
+import os
 
 Array = np.array
 
@@ -26,7 +27,8 @@ class ImageIterator:
 
 class ADEvaluator:
 
-    def __init__(self, dataset, model_name, source, module, model_params, data_dir, normal_cls=0, device='cuda',
+    def __init__(self, dataset, model_name, source, module, model_params, data_dir, normal_cls=0,
+                 device='cuda', cache_dir='cache',
                  things_transform=None, **kwargs):
 
         self.extractor = get_extractor(
@@ -40,6 +42,13 @@ class ADEvaluator:
         self.device = device
         self.things_transform = things_transform
         self.module = module
+
+        variant = ''
+        if "variant" in model_params:
+            variant = model_params['variant']
+        cache_path = f'{dataset}_{model_name}_{variant}_{module}.npz'
+        cache_path = cache_path.replace('/', '-')
+        self.cache_path = os.path.join(cache_dir, cache_path)
 
         if dataset == 'cifar10':
             anomaly_ds = ADCIFAR10(transform=transform, data_dir=data_dir)
@@ -62,18 +71,29 @@ class ADEvaluator:
 
     def evaluate(self, normal_classes, knn_k=5, do_transform=False):
 
-        train_features = self.extractor.extract_features(
-            batches=ImageIterator(self.dataset.train_dataloader()),
-            module_name=self.module,
-            flatten_acts=True
-        )
+        if os.path.exists(self.cache_path):
+            saved_features = np.load(self.cache_path)
+            train_features = saved_features['train']
+            test_features = saved_features['test']
+        else:
+            train_features = self.extractor.extract_features(
+                batches=ImageIterator(self.dataset.train_dataloader()),
+                module_name=self.module,
+                flatten_acts=True
+            )
+            test_features = self.extractor.extract_features(
+                batches=ImageIterator(self.dataset.test_dataloader()),
+                module_name=self.module,
+                flatten_acts=True
+            )
+            np.savez(self.cache_path, train=train_features, test=test_features)
+
+        if do_transform:
+            train_features = self.things_transform.transform_features(train_features)
         train_features = F.normalize(torch.tensor(train_features).to(torch.float32), dim=-1)
 
-        test_features = self.extractor.extract_features(
-            batches=ImageIterator(self.dataset.test_dataloader()),
-            module_name=self.module,
-            flatten_acts=True
-        )
+        if do_transform:
+            test_features = self.things_transform.transform_features(test_features)
         test_features = F.normalize(torch.tensor(test_features).to(torch.float32), dim=-1)
 
         aucs = []
@@ -82,8 +102,6 @@ class ADEvaluator:
             test_reduced, test_reduced_labels = self.dataset.reduce_test(test_embeddings=test_features,
                                                                          normal_cls=normal_cls)
 
-            print(train_reduced.shape)
-            print(test_reduced.shape)
             sim_matrix = torch.mm(test_reduced, train_reduced.t())
             sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
             anomaly_scores = - sim_weight.mean(dim=-1).cpu().numpy()
