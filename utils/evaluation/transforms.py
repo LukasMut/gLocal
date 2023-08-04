6f65796796ee02adb11ffc3f93db29d955053d79
LukasMut <muttenthaler@cbs.mpg.de>
2023-08-04 16:42:18 +0200

    updated glocal transform to include adversarial

--- utils/evaluation/transforms.py
+++ utils/evaluation/transforms.py
@@ -60,8 +60,8 @@ class GlobalTransform:
             if "bias" in self.transform:
                 features += self.transform["bias"]
         elif self.transform.shape[0] != self.transform.shape[1]:
-            weights = self.transform[:,:-1]
-            bias = self.transform[:,-1]
+            weights = self.transform[:, :-1]
+            bias = self.transform[:, -1]
             features = features @ weights + bias
         else:
             features = features @ self.transform
@@ -80,27 +80,26 @@ class GlocalTransform:
     alpha: float = 0.25
     tau: float = 0.1
     contrastive_batch_size: float = 1024
+    adversarial: bool = False
 
     def __post_init__(
         self,
     ) -> None:
-        path_to_transform = os.path.join(
-            *[
-                str(arg)
-                for arg in (
-                    self.root,
-                    self.source,
-                    self.model,
-                    self.module,
-                    self.optim.lower(),
-                    self.eta,
-                    self.lmbda,
-                    self.alpha,
-                    self.tau,
-                    self.contrastive_batch_size,
-                )
-            ]
-        )
+        args = [
+            self.root,
+            self.source,
+            self.model,
+            self.module,
+            self.optim.lower(),
+            self.eta,
+            self.lmbda,
+            self.alpha,
+            self.tau,
+            self.contrastive_batch_size,
+        ]
+        if self.adversarial:
+            args.append("adversarial")
+        path_to_transform = os.path.join(*[str(arg) for arg in args])
         self.transform = self._load_transform(path_to_transform)
 
     @staticmethod
