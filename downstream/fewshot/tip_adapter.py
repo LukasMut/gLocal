TipAdapter normalization fix, small changes, added FS dataset extraction

downstream/fewshot/tip_adapter.py
@@ -9,7 +9,7 @@ class TipAdapter:
     ):
         self.F = F
         self.L = L
-        self.W = W
+        self.W = W / np.linalg.norm(W, axis=1, keepdims=True)
         self.alpha = alpha
         self.beta = beta
 